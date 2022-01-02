0eeceeea5064b82d23c2e3a82358d8906b75f673
Mariotaku <mariotaku.lee@gmail.com>
2022-01-02 22:13:19 +0900

    Add project files.

---
+++ template-native-gui-app.cpp
@@ -0,0 +1,12 @@
+﻿// template-native-gui-app.cpp : Defines the entry point for the application.
+//
+
+#include "template-native-gui-app.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
